ec9449dff429c11c1de29430b95748e4223f31c2
kolbbond <kolbbond@gmail.com>
2024-04-19 13:54:01 -0400

    async startup and catch exceptions

--- main.cpp
+++ main.cpp
@@ -1,14 +1,17 @@
 // matlab example embed from docs
 
 #include "MatlabEngine.hpp"
+#include "MatlabEngine/engine_exception.hpp"
 #include "MatlabEngine/engine_factory.hpp"
 #include "MatlabEngine/matlab_engine.hpp"
 #include "MatlabExecutionInterface/util.hpp"
 #include "MatlabExecutionInterface/value_future.hpp"
+#include <chrono>
 #include <memory>
 #include <sstream>
 #include <stdio.h>
 #include <string>
+#include <thread>
 
 using namespace matlab::engine;
 
@@ -25,14 +28,44 @@ int main() {
   printf("starting matlab\n");
   //  std::unique_ptr<MATLABEngine> matlabPtr = startMATLAB();
 
+  // timer
+  auto tm1 = std::chrono::high_resolution_clock::now();
+
   // async start matlab
   FutureResult<std::unique_ptr<MATLABEngine>> matlabFuture = startMATLABAsync();
   //
   printf("matlab is starting in background\n");
   printf("do other stuff here\n");
+  printf("count a lot\n");
+  auto tc1 = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < 5e6; i++) {
+    printf("\rCount: %i", i);
+    fflush(stdout);
+  }
+  printf("\n");
+  
+  auto tc2 = std::chrono::high_resolution_clock::now();
+  std::cout << "counting took  "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(tc2 - tc1)
+                       .count() /
+                   1000.0
+            << " [s]\n";
+  // sleep async
+  /*
+  int sleep_time = 10;
+  printf("sleeping this thread for [s]: %i\n",sleep_time);
+  std::this_thread::sleep_for(std::chrono::seconds(sleep_time));
+  */
 
   std::unique_ptr<MATLABEngine> matlabPtr = matlabFuture.get();
   printf("GOOD\n");
+
+  auto tm2 = std::chrono::high_resolution_clock::now();
+  std::cout << "matlab startup took "
+            << std::chrono::duration_cast<std::chrono::milliseconds>(tm2 - tm1)
+                       .count() /
+                   1000.0
+            << " [s]\n";
 
   // GAME LOOP!
   //////////////////////////////////////////////////
@@ -48,11 +81,10 @@ int main() {
   matlabPtr->eval(u"workspace;");
 
   while (!done) {
-
     // our string
     printf(">> ");
     std::getline(std::cin, str_use);
-    if (str_use == "exit"){
+    if (str_use == "exit") {
       printf("EXIT NOW\n");
       done = true;
       break;
@@ -67,7 +99,14 @@ int main() {
     auto buf_err = std::make_shared<std::basic_stringbuf<char16_t>>();
 
     // eval does not output a result
-    matlabPtr->eval(str_in, buf_out, buf_err);
+    // catch exceptions?
+    try {
+      matlabPtr->eval(str_in, buf_out, buf_err);
+    } catch (matlab::engine::MATLABExecutionException) {
+      printf("Matlab execution exception!\n");
+    } catch (InterruptedException) {
+      printf("Matlab interrupted exception!\n");
+    }
 
     // print whatever matlab outputted
     // printf("\n");
